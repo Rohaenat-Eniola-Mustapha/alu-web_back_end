6b85b0af946b6f6d9e0d33e4e5be23f65fa3da74
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2025-01-06 17:19:44 +0200

    redo of task 4

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -5,7 +5,7 @@ Module for annotated functions defined and specified with values
 """
 
 
-def define_variables(a:int, pi:float, i_understand_annotations:bool, school:str):
+def define_variables(a: int, pi: float, i_understand_annotations: bool, school: str):
     """Returns the value of the defined variables"""
     a = 1
     pi = 3.14

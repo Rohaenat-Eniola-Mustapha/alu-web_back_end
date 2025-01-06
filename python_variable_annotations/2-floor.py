add5c5897651435ede58573c77a349b36d9aeb0a
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2025-01-06 16:51:09 +0200

    Basic annotations - floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Module for a type-annotated function floor.
+"""
+
+
+import math
+
+def floor(n: float) -> int:
+    """Returns the floor of a float as an integer."""
+    return math.floor(n)

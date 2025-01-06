d9751cad5689ded9f22e8520cd35cc5aa9878148
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2025-01-06 13:32:00 +0200

    Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+Module for a type-annotated function concat.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns the concatenation of two strings."""
+    return str1 + str2

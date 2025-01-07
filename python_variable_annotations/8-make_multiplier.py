30248dde889c9af0f6a34c51a439d74a875210af
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2025-01-07 10:55:29 +0200

    Pycode validation correct

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -9,7 +9,7 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """Returns the function that multiplies a float 
+    """Returns the function that multiplies a float
     by multiplier"""
     def multiplier_func(value: float) -> float:
         return value * multiplier

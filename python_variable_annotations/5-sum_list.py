6bed0038244b2b6202208301bc69f8b051369dff
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2025-01-07 07:23:19 +0200

    Redo of task 5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -5,6 +5,8 @@ Module for a type annotated function sum_list.
 """
 
 
+from typing import List
+
 def sum_list(input_list: list[float]) -> float:
     """Returns their sum as a float"""
     return sum(input_list)

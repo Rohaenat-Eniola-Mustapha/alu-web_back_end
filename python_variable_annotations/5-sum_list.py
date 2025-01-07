9fa18d205edf6239587b07917afbb0bf0c55b2f6
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2025-01-07 07:36:58 +0200

    task 5 & 6

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -7,6 +7,7 @@ Module for a type annotated function sum_list.
 
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     """Returns the sum of a list of floats."""
     return sum(input_list)
--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -7,6 +7,7 @@ Module for a type annotated function sum_mixed_list.
 
 from typing import List, Union
 
+
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Returns the sum of a list containing integers and floats as a float."""
     return sum(mxd_lst)

d883f9c1388daa2b9b2f33b55314e4b23eec36e4
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2025-01-18 09:29:28 +0200

    task 6

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -23,3 +23,23 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
 
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates that the provided password matches the hashed password.
+
+    Args:
+        hashed_password (bytes): The hashed password.
+        password (str): The plain text password to validate.
+
+    Returns:
+        bool: True if the password matches the hash, False otherwise.
+    """
+    if not isinstance(hashed_password, bytes):
+        raise TypeError("Hashed password must be of type bytes")
+    if not isinstance(password, str):
+        raise TypeError("Password must be a string")
+
+    # Use bcrypt to check if the password matches the hashed password
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)

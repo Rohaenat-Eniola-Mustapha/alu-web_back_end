0159df9cac6f8ad5040039da9d148631baa38e90
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2025-02-15 14:39:09 +0200

    task 1

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,9 +1,16 @@
+#!/usr/bin/env python3
 from flask import Flask, render_template
+'''
+Create a single / route and an index.html template
+'''
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    '''
+    returns the template 0-index.html
+    '''
     return render_template('0-index.html')
 
 if __name__ == '__main__':

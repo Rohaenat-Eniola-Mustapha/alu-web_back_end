f30214d73b7b249e2fa572f3427ec6918efa25f9
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2025-02-15 14:58:30 +0200

    task 2

---
+++ i18n/1-app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Flask app with Babel internationalization."""
+
+from flask import Config, Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)  # Instantiate the Babel extension
+
+@app.route('/')
+def index():
+    """Render the 1-index.html template."""
+    return render_template('1-index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)

d6370a548fd626255b4ed48d8129820e599118ba
rmustapha <134163158+Rohaenat-Eniola-Mustapha@users.noreply.github.com>
2025-03-14 18:59:28 +0200

    Update 4-app.py

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -1,45 +1,37 @@
 #!/usr/bin/env python3
-"""
-Flask app with Babel integration to handle i18n
-"""
+''' Flask app '''
 
-from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask import Flask, request, render_template
+from flask_babel import Babel, gettext
+
+app = Flask(__name__)
+babel = Babel(app)
 
 
 class Config:
-    """
-    Configuration for Babel
-    """
+    ''' App config '''
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app = Flask(__name__)
 app.config.from_object(Config)
-babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale():
-    """
-    Determine the best match for language selection.
-    Prioritizes the 'locale' query parameter if it's a supported language.
-    """
+    ''' return best languages '''
     locale = request.args.get('locale')
-    if locale in app.config['LANGUAGES']:
+    if locale:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/')
-def index():
-    """
-    Render the index page.
-    """
+@app.route("/", methods=["GET"], strict_slashes=False)
+def hello_world():
+    ''' return the template '''
     return render_template('4-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()

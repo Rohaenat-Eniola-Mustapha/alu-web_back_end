0d0986cc5a60dd015542da4110efe6db086c7e86
rmustapha <134163158+Rohaenat-Eniola-Mustapha@users.noreply.github.com>
2025-03-14 18:58:49 +0200

    Update 3-app.py

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,54 +1,34 @@
 #!/usr/bin/env python3
-"""
-Flask app with Babel integration for multilingual support.
+''' Flask app '''
 
-This module initializes a Flask application with Babel to provide
-English and French translations.
+from flask import Flask, request, render_template
+from flask_babel import Babel, gettext
 
-Functions:
-    get_locale: Determines the best language for the user.
-"""
-
-
-from flask import Flask, render_template, request
-from flask_babel import Babel, _
-from flask_babel import Babel, _
+app = Flask(__name__)
+babel = Babel(app)
 
 
 class Config:
-    """Configuration class for Flask-Babel."""
+    ''' App config '''
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app = Flask(__name__)
 app.config.from_object(Config)
-
-
-babel = Babel(app)  # Initialize Babel
 
 
 @babel.localeselector
 def get_locale():
-    """
-    Determine the best match for
-    supported languages based on the request.
-    """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    ''' return best languages '''
+    return request.accept_languages.best_match(Config.LANGUAGES)
 
 
-@app.route('/')
-def index():
-    """
-    Render the 3-index.html template with translations.
-    """
-    return render_template(
-        '3-index.html',
-        home_title=_("home_title"),
-        home_header=_("home_header")
-        )
+@app.route("/", methods=["GET"], strict_slashes=False)
+def hello_world():
+    ''' return the template '''
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
